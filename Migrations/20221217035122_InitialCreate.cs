28049cb197a1bb3768247e02b1675c990428c5ff
Benjamin Walsh <benjamin.walsh1@dcmail.ca>
2022-12-16 23:43:12 -0500

    Add project files.

---
+++ Migrations/20221217035122_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NbaPlayersApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    team = table.Column<string>(nullable: true),
+                    fname = table.Column<string>(nullable: true),
+                    lname = table.Column<string>(nullable: true),
+                    ppg = table.Column<double>(nullable: false),
+                    apg = table.Column<double>(nullable: false),
+                    rpg = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "players");
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NbaPlayersApp.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public String team { get; set; }
+
+        public String fname { get; set; }
+
+        public String lname { get; set; }
+
+        public double ppg { get; set; }
+
+        public double apg { get; set; }
+
+        public double rpg { get; set; }
+
+        public Player()
+        {
+
+        }
+
+    }
+}
